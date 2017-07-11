b6480c4c0427a7dfba344f94e6dee570604331aa
hannes <hannes.bohnengel@tum.de>
2017-07-11 11:16:43 +0200

    renamed these files

--- haptics-showroom-V02.cpp
+++ haptics-showroom-V02.cpp
@@ -1,43 +1,11 @@
 //==============================================================================
 /*
-Software License Agreement (BSD License)
-Copyright (c) 2003-2016, CHAI3D.
-(www.chai3d.org)
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
-
-* Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above
-copyright notice, this list of conditions and the following
-disclaimer in the documentation and/or other materials provided
-with the distribution.
-
-* Neither the name of CHAI3D nor the names of its contributors may
-be used to endorse or promote products derived from this software
-without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-
-\author    <http://www.chai3d.org>
-\author    Francois Conti
-\version   3.1.0 $Rev: 1659 $
+	Filename:	haptics_showroom-V02.cpp
+	Project:	Haptics Showroom
+    Authors:	Naina Dhingra, Ke Xu, Hannes Bohnengel 
+    Revision:	0.2
+	Remarks:	These files are tracked with git and are available on the github
+				repository: https://github.com/hannesb0/haptics-showroom
 */
 //==============================================================================
 
@@ -142,15 +110,7 @@ void processEvents();
 
 
 //==============================================================================
-/*
-DEMO:   01-cube.cpp
-
-This example illustrates how to build a small mesh cube.
-The applications also presents the use of texture properties by defining
-a texture image and associated texture coordinates for each of the vertices.
-A bump map is also used in combination with a shader to create a more
-realistic rendering of the surface.
-*/
+// MAIN FUNCTION
 //==============================================================================
 
 int main(int argc, char **argv)
