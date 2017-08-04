358623a68bf5ceb887d35aad7ea5d869ed903951
hannesb0 <hannesbohny@yahoo.de>
2017-08-04 13:23:09 +0200

    changed room height

--- Global.h
+++ Global.h
@@ -43,7 +43,7 @@ struct MyOrientation{
 // size of the room
 const double roomLength = 6.0;	// x-axis
 const double roomWidth = 4.0;	// y-axis
-const double roomHeight = 2.0;	// z-axis
+const double roomHeight = 2.5;	// z-axis
 
 // default filename of image for graphical display 
 const string defaultTextureImage = "brick-color.png";
