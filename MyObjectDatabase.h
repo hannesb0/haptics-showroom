a96e63426de7df713843e817a056dc0cf54687b3
hannesb0 <hannesbohny@yahoo.de>
2017-08-06 16:26:14 +0200

    inserted new objects

--- MyObjectDatabase.h
+++ MyObjectDatabase.h
@@ -46,32 +46,42 @@ Audio:
 
 */
 
-MyProperties Cube_Aluminium("G1RhombAluminumMesh.JPG", "G1RhombAluminumMeshNormal.JPG", "RhombicAluminumMesh_3_Z.wav", cVector3d(0.3, 0.3, 0.3), orientation1, MyShape(cube),
+MyProperties Cube_Aluminium("G1RhombAluminumMesh.JPG", "G1RhombAluminumMeshNormal.JPG", "RhombicAluminumMesh_3_Z.wav", cVector3d(0.5, 0.35, 0.01), orientation1, MyShape(cube),
+	3, 0.8, 0.3, 0.3, 0.2, 1.0, 0.2, 0.8);
+
+MyProperties Cube_SilverOak("G4WoodTypeSilverOak.JPG", "G4WoodTypeSilverOakNormal.JPG", "CompressedWood_3_Z.wav", cVector3d(0.5, 0.35, 0.01), orientation1, MyShape(cube),
+	3, 0.6, 0.2, 0.2, 0.1, 1.0, 0.2, 0.8);
+
+MyProperties Cube_CrushedRock("G2CrushedRock.JPG", "G2CrushedRockNormal.JPG", "CrushedRock_3_Z.wav", cVector3d(0.5, 0.35, 0.01), orientation1, MyShape(cube),
+	3, 1.0, 0.1, 0.1, 0.1, 1.0, 0.2, 0.8);
+
+MyProperties Cube_Steel("G3StainlessSteel.JPG", "G3StainlessSteelNormal.JPG", "StainlessSteel_5_Y.wav", cVector3d(0.5, 0.35, 0.01), orientation1, MyShape(cube),
+	3, 0.8, 0.1, 0.1, 0.2, 1.0, 0.2, 0.8);
+
+MyProperties Cube_WoodProfiled("G4ProfiledWoodPlate.JPG", "G4ProfiledWoodPlateNormal.JPG", "ProfiledWoodenPlate_3_Z.wav", cVector3d(0.5, 0.35, 0.01), orientation1, MyShape(cube),
+	3, 0.6, 0.3, 0.3, 0.2, 1.0, 0.2, 0.8);
+
+MyProperties Cube_Rubber("G5ProfiledRubberPlate.JPG", "G5ProfiledRubberPlateNormal.JPG", "ProfiledRubberPlate_3_Z.wav", cVector3d(0.5, 0.35, 0.01), orientation1, MyShape(cube),
+	3, 0.6, 0.9, 0.9, 0.3, 1.0, 0.2, 0.8);
+
+
+MyProperties Cube_GlowingMetal("heat1.jpg", "", "", cVector3d(0.5, 0.35, 0.01), orientation1, MyShape(cube),
+	5, 0.7, 0.5, 0.5, 0.01, 0.0, 0.0, 0.0);
+
+MyProperties Cube_Lava("lava.jpg", "lavaNormal.jpg", "", cVector3d(0.5, 0.35, 0.01), orientation1, MyShape(cube),
+	4, 0.5, 0.5, 0.5, 0.01, 0.0, 0.0, 0.0);
+
+MyProperties Cube_Granite("G2GraniteTypeVeneziano.jpg", "", "", cVector3d(0.5, 0.35, 0.01), orientation1, MyShape(cube),
+	2, 1.0, 0.01, 0.01, 0.01, 0.0, 0.0, 0.0);
+
+MyProperties Cube_Ice("frost.jpg", "frostNormal.jpg", "", cVector3d(0.5, 0.35, 0.01), orientation1, MyShape(cube),
+	1, 0.8, 0.01, 0.01, 0.01, 0.0, 0.0, 0.0);
+
+MyProperties Cube_Coarsefoam("G7CoarseFoam.JPG", "G7CoarseFoam_NRM.JPG", "CoarseFoam_3_Z.wav", cVector3d(0.2, 0.2, 0.1), orientation1, MyShape(cube),
 	3, 0.6, 0.1, 0.1, 0.1, 1.0, 0.8, 0.8);
 
-MyProperties Sphere_Steel("G3StainlessSteel.JPG", "G3StainlessSteelNormal.JPG", "StainlessSteel_5_Y.wav", cVector3d(0.1, 0.1, 0.1), orientation1, MyShape(sphere),
-	3, 0.7, 0.1, 0.1, 0.2, 1.0, 0.2, 0.8);
-
-MyProperties Cylinder_Coarsefoam("G7CoarseFoam.JPG", "G7CoarseFoam_NRM.JPG", "CoarseFoam_3_Z.wav", cVector3d(0.2, 0.2, 0.1), orientation1, MyShape(cube),
-	3, 0.6, 0.1, 0.1, 0.1, 1.0, 0.8, 0.8);
-
-MyProperties Cube_WoodProfiled("G4ProfiledWoodPlate.JPG", "G4ProfiledWoodPlateNormal.JPG", "ProfiledWoodenPlate_3_Z.wav", cVector3d(0.2, 0.2, 0.4), orientation1, MyShape(cube),
-	3, 0.6, 0.4, 0.4, 0.2, 1.0, 0.2, 0.8);
-
-MyProperties Cube_Rubber("G5ProfiledRubberPlate.JPG", "G5ProfiledRubberPlateNormal.JPG", "ProfiledRubberPlate_3_Z.wav", cVector3d(0.2, 0.2, 0.5), orientation1, MyShape(cylinder),
-	3, 0.9, 0.8, 0.8, 0.2, 1.0, 0.2, 0.8);
-
-MyProperties Cube_CrushedRock("G2CrushedRock.JPG", "G2CrushedRockNormal.JPG", "CrushedRock_3_Z.wav", cVector3d(0.3, 0.3, 0.3), orientation1, MyShape(cube),
-	3, 0.6, 0.1, 0.1, 0.1, 1.0, 0.8, 0.8);
-
-MyProperties Cube_SilverOak("G4WoodTypeSilverOak.JPG", "G4WoodTypeSilverOakNormal.JPG", "CompressedWood_3_Z.wav", cVector3d(0.4, 0.4, 0.4), orientation1, MyShape(cube),
-	3, 0.6, 0.1, 0.1, 0.1, 1.0, 0.8, 0.8);
-
-MyProperties Cube_Ice("frost.jpg", "frostNormal.jpg", "", cVector3d(0.4, 0.4, 0.2), orientation1, MyShape(cube),
-	1, 0.8, 0.1, 0.1, 0.1, 0.0, 0.8, 0.8);
-
-MyProperties Cube_Lava("heat1.jpg", "heat1Normal.jpg", "", cVector3d(0.4, 0.4, 0.2), orientation1, MyShape(sphere),
-	5, 0.8, 0.1, 0.1, 0.1, 0.0, 0.8, 0.8);
+MyProperties Property_3ds("whiteWall.jpg", "whiteWallNormal.png", "", cVector3d(0.2, 0.2, 0.4), orientation1, MyShape(cube),
+	3, 0.6, 0.4, 0.4, 0.2, 0.0, 0.2, 0.8);
 
 
 //------------------------------------------------------------------------------
@@ -81,62 +91,61 @@ MyProperties Cube_Lava("heat1.jpg", "heat1Normal.jpg", "", cVector3d(0.4, 0.4, 0
 MyProperties myFloor("floor1small.jpg", "", "", cVector3d(roomLength, roomWidth, 0.0), MyOrientation{ cVector3d(0.0, 0.0, 0.0), 0.0 }, MyShape(plane),
 	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
-MyProperties myCeiling("Ceiling.jpg", "", "", cVector3d(roomLength, roomWidth, 0.0), MyOrientation{ cVector3d(0.0, 1.0, 0.0), 180.0 }, MyShape(plane),
+MyProperties myCeiling("ceiling1.jpg", "", "", cVector3d(roomLength, roomWidth, 0.0), MyOrientation{ cVector3d(0.0, 1.0, 0.0), 180.0 }, MyShape(plane),
 	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
-MyProperties myFrontWall("sand-wall.png", "", "", cVector3d(roomHeight, roomWidth, 0.0), MyOrientation{ cVector3d(0.0, 1.0, 0.0), -90.0 }, MyShape(plane),
+MyProperties myFrontWall("whiteWall.jpg", "", "", cVector3d(roomHeight, roomWidth, 0.0), MyOrientation{ cVector3d(0.0, 1.0, 0.0), -90.0 }, MyShape(plane),
 	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
-MyProperties myBackWall("sand-wall.png", "", "", cVector3d(roomHeight, roomWidth, 0.0), MyOrientation{ cVector3d(0.0, 1.0, 0.0), 90.0 }, MyShape(plane),
+MyProperties myBackWall("whiteWall.jpg", "", "", cVector3d(roomHeight, roomWidth, 0.0), MyOrientation{ cVector3d(0.0, 1.0, 0.0), 90.0 }, MyShape(plane),
 	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
-MyProperties myLeftWall("sand-wall.png", "", "", cVector3d(roomLength, roomHeight, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), -90.0 }, MyShape(plane),
+MyProperties myLeftWall("whiteWall.jpg", "", "", cVector3d(roomLength, roomHeight, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), -90.0 }, MyShape(plane),
 	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
-MyProperties myRightWall("sand-wall.png", "", "", cVector3d(roomLength, roomHeight, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), 90.0 }, MyShape(plane),
+MyProperties myRightWall("whiteWall.jpg", "", "", cVector3d(roomLength, roomHeight, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), 90.0 }, MyShape(plane),
 	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
-MyProperties myWindow("window1.jpg", "", "", cVector3d(0.75, 1.25, 0.0), MyOrientation{ cVector3d(0.0, 1.0, 0.0), 90.0 }, MyShape(plane),
+MyProperties myWindow("window1.jpg", "", "", cVector3d(0.6, 0.8, 0.0), MyOrientation{ cVector3d(0.0, 1.0, 0.0), 90.0 }, MyShape(plane),
 	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
+
+
+//------------------------------------------------------------------------------
+// Labels
+//------------------------------------------------------------------------------	
 
 MyProperties myTitle("title.jpg", "", "", cVector3d(0.3, 1.32, 0.0), MyOrientation{ cVector3d(0.0, 1.0, 0.0), 90.0 }, MyShape(plane),
 	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
+MyProperties myLeftLablePW("ProfiledWood.png", "", "", cVector3d(0.615, 0.15, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), -90.0 }, MyShape(plane),
+	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
+MyProperties myLeftLableSS("StainlessSteel.png", "", "", cVector3d(0.615, 0.15, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), -90.0 }, MyShape(plane),
+	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
+MyProperties myLeftLableCR("CrushedRock.png", "", "", cVector3d(0.615, 0.15, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), -90.0 }, MyShape(plane),
+	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
+MyProperties myLeftLableSO("SilverOak.png", "", "", cVector3d(0.615, 0.15, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), -90.0 }, MyShape(plane),
+	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
+MyProperties myLeftLableAM("AluminiumMesh.png", "", "", cVector3d(0.615, 0.15, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), -90.0 }, MyShape(plane),
+	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
+MyProperties myRightLablePR("ProfiledRubber.png", "", "", cVector3d(0.615, 0.15, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), 90.0 }, MyShape(plane),
+	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
+MyProperties myRightLableGS("GlowingSteel.png", "", "", cVector3d(0.615, 0.15, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), 90.0 }, MyShape(plane),
+	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
+MyProperties myRightLableSL("StiffenedLava.png", "", "", cVector3d(0.615, 0.15, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), 90.0 }, MyShape(plane),
+	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
+MyProperties myRightLableCG("ColdGranite.png", "", "", cVector3d(0.615, 0.15, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), 90.0 }, MyShape(plane),
+	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
+MyProperties myRightLableFW("FrozenWater.png", "", "", cVector3d(0.615, 0.15, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), 90.0 }, MyShape(plane),
+	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
 
-/*
-MyProperties Cube_Aluminium("G1RhombAluminumMesh.JPG", "G1RhombAluminumMeshNormal.JPG", "metal-scraping.wav", cVector3d(0.3, 0.3, 0.3), orientation2, MyShape(cube),
-3, 0.6, 0.1, 0.1, 0.1, 1.0, 0.8, 0.8);
-
-MyProperties Sphere_Steel("G3StainlessSteel.JPG", "G3StainlessSteelNormal.JPG", "metal-scraping.wav", cVector3d(0.3, 0.3, 0.3), orientation1, MyShape(sphere),
-4, 0.7, 0.1, 0.1, 0.2, 1.0, 0.2, 0.8);
-
-MyProperties Cylinder_Granite("G2GraniteTypeVeneziano.JPG", "G2GraniteTypeVenezianoNormal.JPG", "wood-scraping.wav", cVector3d(0.2, 0.2, 0.5), orientation1, MyShape(cylinder),
-3, 0.9, 0.8, 0.8, 0.2, 1.0, 0.2, 0.8);
-
-MyProperties Cube_WoodProfiled("G4ProfiledWoodPlate.JPG", "G4ProfiledWoodPlateNormal.JPG", "compressedWood.wav", cVector3d(0.2, 0.2, 0.4), orientation1, MyShape(cube),
-2, 0.6, 0.4, 0.4, 0.2, 1.0, 0.2, 0.8);
-
-MyProperties Rubber("G5ProfiledRubberPlate.JPG", "G5ProfiledRubberPlateNormal.JPG", 3, 0.5, 0.2, 0.2, 0.2, 0, 0.2);
-
-MyProperties Rock("G2CrushedRock.JPG", "G2CrushedRockNormal.JPG", 3, 0.5, 0.2, 0.2, 0.2, 0, 0.2);
-
-MyProperties WoodCompressed("G4CompressedWoodVersion1.JPG", "G4CompressedWoodVersion1Normal.JPG", 3, 0.5, 0.2, 0.2, 0.2, 0, 0.2);
-
-
-
-MyProperties WoodOak("G4WoodTypeSilverOak.JPG", "G4WoodTypeSilverOakNormal.JPG", 3, 0.5, 0.2, 0.2, 0.2, 0, 0.2);
-// ->
-
-
-
-MyProperties Foam("G7CoarseFoam.JPG", "G7CoarseFoamNormal.JPG", 3, 0.5, 0.2, 0.2, 0.2, 0, 0.2);
-*/
+MyProperties myRightLableCaution("CautionHot.jpg", "", "", cVector3d(0.3, 0.22, 0.0), MyOrientation{ cVector3d(1.0, 0.0, 0.0), 90.0 }, MyShape(plane),
+	3, 0.8, 0.1, 0.1, 0.1, 0.0, 0.0, 0.0);
